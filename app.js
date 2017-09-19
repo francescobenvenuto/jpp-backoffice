6b3d0e7594e6387f79e6ea89e81b603aa9f31d1f
Francesco <benvenutofrancesco@gmail.com>
2017-09-19 17:01:10 +0200

    add downloads page

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+// setup 
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+
+// routes
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+
+app.get("/downloads", function(req, res){
+	var downloads = [
+		{name: "PDF 1", image: "#"},
+		{name: "PDF 2", image: "#"},
+		{name: "PDF 3", image: "#"}
+	]
+	res.render("downloads",{downloads:downloads});
+});
 
 
 
 
 
-
-
-
-
-
-
+// listening 
 app.listen(3001, 'localhost', function() {
-  console.log("... port %d in %s mode", app.address().port, app.settings.env);
+  console.log("server has started!!!");
 });
