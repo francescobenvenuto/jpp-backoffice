40fc7da9e36522fabd1c18867bf820ae68d2f60d
Francesco <benvenutofrancesco@gmail.com>
2017-09-19 15:33:49 +0200

    add express and ejs packages

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("see if it works");
+var express = require("express");
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, 'localhost', function() {
+  console.log("... port %d in %s mode", app.address().port, app.settings.env);
+});
