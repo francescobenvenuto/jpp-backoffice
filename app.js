763656c6a8e0bf5cfc766a6645d1ae96b50acf29
Francesco <benvenutofrancesco@gmail.com>
2017-09-21 12:41:29 +0200

    add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,77 @@
 // setup 
-var express = require("express");
-var app = express();
-var path = require("path");
+var express    = require("express"),
+	app        = express(),
+    path       = require("path"),
+	bodyParser = require("body-parser"),
+	mongoose   = require("mongoose")
 
+
+// ES6 promises
+mongoose.Promise = Promise;
+
+// mongodb connection
+mongoose.connect("mongodb://localhost/jpp", {
+  useMongoClient: true,
+  promiseLibrary: global.Promise
+});
+
+var db = mongoose.connection;
+
+// mongodb error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// mongodb connection open
+db.once('open', () => {
+  console.log(`Connected to Mongo at: ${new Date()}`)
+});
+
+
+// DATABASE SCHEMA SETUP
+
+var jppSchema = new mongoose.Schema({
+	name: String,
+	lastname: String,
+	email: String,
+	website: String,
+	telephone: String,
+	indirizzo: String,
+	numeroPartitaIva: String,
+	password: String
+});
+
+
+
+// Jpp.create(
+// 	{
+// 		name: "Francesco",
+// 		lastname: "Benvenuto",
+// 		email: "benvenutofrancesco@gmail.com",
+// 		website: "http://www.francescobenvenuto.com",
+// 		telephone: "00393495765703",
+// 		indirizzo: "Via Paolo Sarpi 5",
+// 		numeroPartitaIva: "IT0123456789",
+// 		password: "test"
+
+// }, function(err, jpp){
+// 	if(err){
+// 		console.log(err);
+// 	}
+// 		else {
+// 			console.log("New user created: ");
+// 			console.log(jpp);
+// 		}
+// });
+
+var Jpp = mongoose.model("Jpp", jppSchema);
+
+
+
+
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
